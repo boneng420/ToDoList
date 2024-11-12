90b7ebe2b22179c457b30863f4d0df7d64487a58
boneng420 <2202137@ub.edu.ph>
2024-11-12 17:21:47 +0800

    mobile

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View, SafeAreaView } from 'react-native';
+import ToDoScreen from './components/ToDoScreen';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <StatusBar />
+        <ToDoScreen />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
